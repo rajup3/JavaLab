eb34b0530380cbdd500d5ccb7d299cbbc8da1de7
rajup3 <rajupatel.tech@gmail.com>
2013-12-20 13:05:41 +0530

    change

--- JavaLab/src/com/StaticTest.java
+++ JavaLab/src/com/StaticTest.java
@@ -7,8 +7,10 @@ public class StaticTest {
 	 */
 	public static void main(String[] args) {
 
-		System.out.println(" >>>  ");
+		System.out.println(" static test starts  ");
 		
+		
+		System.out.println(" static test ends  ");
 	}
 
 }
