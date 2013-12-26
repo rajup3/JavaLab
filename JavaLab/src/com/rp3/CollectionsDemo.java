44fd48484b6c960b6ec7f8e39f6c4ab6a311f06d
rajup3 <rajupatel.tech@gmail.com>
2013-12-26 14:42:17 +0530

    collection change

--- JavaLab/src/com/rp3/CollectionsDemo.java
+++ JavaLab/src/com/rp3/CollectionsDemo.java
@@ -1,5 +1,9 @@
 package com.rp3;
 
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+
 
 public class CollectionsDemo {
 
@@ -18,7 +22,14 @@ public class CollectionsDemo {
 			// vector
 			
 			
-			// hastable
+			// hashtable
+			Hashtable<Object, Object> env = new Hashtable<Object, Object>();
+			env.put("ADMIN_EMAIL_ID", "admin@emailtest.com");
+		    Iterator<Map.Entry<Object, Object>> it = env.entrySet().iterator();
+		    while(it.hasNext()){
+		    	Map.Entry<Object, Object> entry = it.next();
+		    	System.out.println(" KEY: "+entry.getKey()+", VALUE: "+entry.getValue());
+		    }
 			
 			
 			// map
